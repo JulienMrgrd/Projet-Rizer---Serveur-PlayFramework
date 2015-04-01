8495677561dede67e32822b1a45dafb7d81fe0ee
JulienMrgrd <julien.margarido@gmail.com>
2015-04-01 18:07:06 +0200

    commit encode

---
+++ app/service/SocialActivityService.java
@@ -0,0 +1,23 @@
+package service;
+
+public class SocialActivityService implements SocialActivityServiceInterface{
+
+	@Override
+	public boolean followArtist(Integer idProfil, Integer idArtist) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean unfollowArtist(Integer idProfil, Integer idArtist) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean rizeFuturArtist(Integer idProfil, Integer idFuturArtist) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
