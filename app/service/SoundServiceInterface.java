8f44ea4642c75922f0e09acc53faea7c080422fc
JulienMrgrd <julien.margarido@gmail.com>
2015-04-01 17:47:58 +0200

    commit encoding

--- app/service/SoundServiceInterface.java
+++ app/service/SoundServiceInterface.java
@@ -5,45 +5,14 @@ import models.Music;
 public interface SoundServiceInterface {
 
 	
-	/**
-	 * Ajoute une musique en base
-	 * @param idProfil l'id du Profil (Artist ou FuturArtist) qui rajoute cette musique
-	 * @param music le fichier a uploader
-	 * @return vrai si l'operation s'est bien passee
-	 */
 	public boolean addMusic(Integer idProfil, Music music);
 	
-	/**
-	 * Remplace les informations d'une musique en base, si elle existe et qu'elle appartient a ce compte
-	 * @param idProfil l'id du Profil (Artist ou FuturArtist) qui change les informations de la musique
-	 * @param music la musique a remplacer
-	 * @return vrai si l'operation s'est bien passee
-	 */
 	public boolean modifyInfoSound(Integer idProfil, Music music);
 	
-	/**
-	 * Supprime la musique en base
-	 * @param idProfil l'id du Profil qui supprime cette musique
-	 * @param idMusic l'id de la musique a supprimer
-	 * @return vrai si l'operation s'est bien passee
-	 */
 	public boolean deleteMusic(Integer idProfil, Integer idMusic);
 	
-	/**
-	 * Ajoute un like a la music
-	 * @param idProfil l'id du profil qui like la musique
-	 * @param idMusic l'id de la musique like
-	 * @return vrai si l'operation s'est bien passee.
-	 */
 	public boolean likerMusic(Integer idProfil, Integer idMusic);
 	
-	/**
-	 * Ajoute un commentaire a une musique
-	 * @param idProfil l'id du Profil qui commente
-	 * @param idMusic l'id de la musique qui est commentee
-	 * @param comment le commentaire
-	 * @return
-	 */
 	public boolean commentMusic(Integer idProfil, Integer idMusic, String comment);
 	
 }
--- app/service/UtilsServiceInterface.java
+++ app/service/UtilsServiceInterface.java
@@ -9,46 +9,16 @@ import models.Music;
 public interface UtilsServiceInterface {
 
 	
-	/**
-	 * Recherche un artiste dans la base et dans la base de Gracenote
-	 * @param research La chaine de caractere
-	 * @return la liste des resultats
-	 */
 	public List<Artist> searchArtist(String research);
 	
-	/**
-	 * Recherche un album dans la base et dans la base de Gracenote
-	 * @param research La chaine de caractere
-	 * @return la liste des resultats
-	 */
 	public List<Album> searchAlbum(String research);
 	
-	/**
-	 * Recherche un musique dans la base et dans la base de Gracenote
-	 * @param research La chaine de caractere
-	 * @return la liste des resultats
-	 */
 	public List<Music> searchMusic(String research);
 	
-	/**
-	 * Retourne la liste des artistes recommandes pour ce profil
-	 * @param idProfil l'id du profil
-	 * @return la liste des resultats
-	 */
 	public List<Artist> recommendArtist(Integer idProfil);
 	
-	/**
-	 * Retourne la liste des albums recommandes pour ce profil
-	 * @param idProfil l'id du profil
-	 * @return la liste des resultats
-	 */
 	public List<Album> recommendAlbum(Integer idProfil);
 	
-	/**
-	 * Retourne la liste des musiques recommandees pour ce profil
-	 * @param idProfil l'id du profil
-	 * @return la liste des resultats
-	 */
 	public List<Music> recommendMusic(Integer idProfil);
 	
 }
