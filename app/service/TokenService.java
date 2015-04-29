da4a1de170ecd7768e46598cb25f82432d4ec5f6
j.margarido <j.margarido@TEAMNET-234.DomaineTeamnet.fr>
2015-04-29 14:59:24 +0200

    Ajout d'un service pour la classe Token

---
+++ app/service/TokenService.java
@@ -0,0 +1,24 @@
+package service;
+
+public class TokenService {
+
+	/**
+	 * Permet de se connecter
+	 * @param login de l'utilisateur
+	 * @param password de l'utilisateur
+	 * @return l'id du Account correspondant
+	 */
+	public String connection(String login, String password) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/**
+	 * Permet de se deconnecter
+	 */
+	public static void logOut(String UUID) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
