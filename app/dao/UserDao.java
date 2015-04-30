5930d370f3fdf6c2bb1c51ccf58850fcb6a32c0a
Venom7 <sinthujan.sivanathan@gmail.com>
2015-04-30 15:26:58 +0200

    dao USER/ACCOUNT/ARTIST

--- app/dao/UserDao.java
+++ app/dao/UserDao.java
@@ -1,5 +1,23 @@
 package dao;
 
+import org.joda.time.DateTime;
+
+import models.*;
+import modelsMongo.PlayJongo;
+
 public class UserDao {
+
+	public static String inscriptionUser(User compte){
+		PlayJongo.getCollection("User").save(compte);
+		return TokenDao.getNewToken(compte.getLogin(), compte.getPassword());
+	}
+
+	public static boolean checkLoginUser(String login){
+		return (PlayJongo.getCollection("User").findOne("{login:#}", login).as(Artist.class) == null);
+	}
+	
+	public static void deleteUser(String _id){
+		PlayJongo.getCollection("User").remove("{ _id:#}", _id);
+	}
 
 }
